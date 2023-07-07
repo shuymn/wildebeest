4cf2ab3553c4ef3ffb8dbae379be484be352b50d
shuymn <mail@shuymn.me>
2023-07-08 08:51:03 +0900

    Add actor_preferences table

---
+++ migrations/0011_add_actor_preference.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS actor_preferences (
+    id TEXT PRIMARY KEY,
+    posting_default_visibility TEXT NOT NULL DEFAULT 'public',
+    posting_default_sensitive INTEGER NOT NULL DEFAULT 0,
+    posting_default_language TEXT,
+    reading_expand_media TEXT NOT NULL DEFAULT 'default',
+    reading_expand_spoilers INTEGER NOT NULL DEFAULT 0,
+
+    FOREIGN KEY(id) REFERENCES actors(id) ON DELETE CASCADE
+);
+
+INSERT INTO actor_preferences SELECT id, 'public', 0, null, 'default', 0 FROM actors;
