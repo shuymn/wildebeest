07d0233f1d51c196247a0cb10ef4e5a2b19ad9c8
shuymn <mail@shuymn.me>
2023-07-09 21:44:00 +0900

    Re-enable eslint rules that can be fixed with minor changes or auto-fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -37,12 +37,5 @@ module.exports = {
 		'@typescript-eslint/restrict-plus-operands': 'off',
 		'@typescript-eslint/restrict-template-expressions': 'off',
 		'@typescript-eslint/no-unnecessary-type-assertion': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-inferrable-types': 'off',
-		'@typescript-eslint/no-non-null-assertion': 'off',
-		'@typescript-eslint/ban-ts-comment': 'off',
-		'@typescript-eslint/no-empty-function': 'off',
-		'@typescript-eslint/ban-types': 'off',
-		'@typescript-eslint/no-empty-interface': 'off',
 	},
 }
