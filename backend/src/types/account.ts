7a8c52f4f5fcbada984001e7d4d75995da33cbd7
shuymn <mail@shuymn.me>
2023-07-24 23:29:08 +0900

    Make the `Relationship` match the specification

--- backend/src/types/account.ts
+++ backend/src/types/account.ts
@@ -32,6 +32,19 @@ export interface MastodonAccount {
 // https://github.com/mastodon/mastodon-android/blob/master/mastodon/src/main/java/org/joinmastodon/android/model/Relationship.java
 export type Relationship = {
 	id: string
+	following: boolean
+	showing_reblogs: boolean
+	notifying: boolean
+	languages?: string[]
+	followed_by: boolean
+	blocking: boolean
+	blocked_by: boolean
+	muting: boolean
+	muting_notifications: boolean
+	requested: boolean
+	domain_blocking: boolean
+	endorsed: boolean
+	note: string
 }
 
 export type Privacy = 'public' | 'unlisted' | 'private' | 'direct'
