15fa39955c3bc43bfa01484ac75b3f7041ce8092
shuymn <mail@shuymn.me>
2023-07-27 00:06:42 +0900

    Add handling for `client_credentials` grant type

--- functions/api/v1/apps/verify_credentials.ts
+++ functions/api/v1/apps/verify_credentials.ts
@@ -2,48 +2,43 @@
 
 import { getVAPIDKeys } from 'wildebeest/backend/src/config'
 import { type Database } from 'wildebeest/backend/src/database'
-import * as errors from 'wildebeest/backend/src/errors'
-import { getClientById } from 'wildebeest/backend/src/mastodon/client'
+import { notAuthorized } from 'wildebeest/backend/src/errors'
+import { Application } from 'wildebeest/backend/src/mastodon'
+import { getClientByClientCredential, getClientById } from 'wildebeest/backend/src/mastodon/client'
 import { VAPIDPublicKey } from 'wildebeest/backend/src/mastodon/subscription'
 import type { ContextData, Env } from 'wildebeest/backend/src/types'
 import { cors } from 'wildebeest/backend/src/utils/cors'
+import { makeJsonResponse, MastodonApiResponse } from 'wildebeest/backend/src/utils/http'
 import type { JWK } from 'wildebeest/backend/src/webpush/jwk'
-
-export type CredentialApp = {
-	name: string
-	website: string
-	vapid_key: string
-}
 
 const headers = {
 	...cors(),
 	'content-type': 'application/json; charset=utf-8',
 }
 
-export const onRequest: PagesFunction<Env, any, ContextData> = async ({ request, env }) => {
-	return handleRequest(env.DATABASE, request, getVAPIDKeys(env))
+type Dependencies = {
+	db: Database
+	vapidKeys: JWK
 }
 
-export async function handleRequest(db: Database, request: Request, vapidKeys: JWK) {
-	if (request.method !== 'GET') {
-		return new Response('', { status: 400 })
+export const onRequestGet: PagesFunction<Env, '', ContextData> = async ({ request, env }) => {
+	const token = request.headers.get('Authorization')?.replace('Bearer ', '')
+	if (token) {
+		return handleRequest({ db: env.DATABASE, vapidKeys: getVAPIDKeys(env) }, token)
 	}
+	return notAuthorized('the access token is invalid')
+}
 
-	const authHeader = request.headers.get('Authorization')?.replace('Bearer ', '')
-	const parts = authHeader?.split('.') ?? ''
-	const clientId = parts[0]
-
-	const client = await getClientById(db, clientId)
+export async function handleRequest(
+	{ db, vapidKeys }: Dependencies,
+	token: string
+): Promise<MastodonApiResponse<Omit<Application, 'client_id' | 'client_secret'>>> {
+	const client = (await getClientById(db, token.split('.')[0])) ?? (await getClientByClientCredential(db, token))
 	if (client === null) {
-		return errors.clientUnknown()
+		return notAuthorized('the access token is invalid')
 	}
-	const vapidKey = VAPIDPublicKey(vapidKeys)
-
-	const res = {
-		name: client.name,
-		website: client.website,
-		vapid_key: vapidKey,
-	}
-
-	return new Response(JSON.stringify(res), { headers })
+	return makeJsonResponse(
+		{ name: client.name, website: client.website, vapid_key: VAPIDPublicKey(vapidKeys) },
+		{ headers }
+	)
 }
---
+++ migrations/0014_add_client_credentials.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS client_credentials (
+  id TEXT PRIMARY KEY,
+  client_id TEXT NOT NULL,
+  access_token TEXT NOT NULL,
+  scopes TEXT NOT NULL,
+  cdate DATETIME NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
+
+  FOREIGN KEY(client_id) REFERENCES clients(id) ON DELETE CASCADE
+)
