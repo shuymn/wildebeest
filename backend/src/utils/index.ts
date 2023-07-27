5d1f4a39a84bef42915412365d2b10252e879809
shuymn <mail@shuymn.me>
2023-07-27 23:47:06 +0900

    Move isUUID function

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -3,3 +3,7 @@ import * as myz from './zod'
 export { myz }
 export * from './cors'
 export * from './http'
+
+export function isUUID(str: string): boolean {
+	return /^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$/i.test(str)
+}
