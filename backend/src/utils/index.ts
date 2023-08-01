c18bf5be02b883922b0541300a8b9ed98dcb385e
shuymn <mail@shuymn.me>
2023-08-01 16:38:39 +0900

    Add utils

--- backend/src/utils/index.ts
+++ backend/src/utils/index.ts
@@ -7,3 +7,7 @@ export * from './http'
 export function isUUID(str: string): boolean {
 	return /^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$/i.test(str)
 }
+
+export function unique<T>(arr: T[]): T[] {
+	return [...new Set(arr)]
+}
