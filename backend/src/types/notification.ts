f7e733f9188bc5947397df266e5d1ed5fcf32351
shuymn <mail@shuymn.me>
2023-08-14 00:20:22 +0900

    Update notification-related types

--- backend/src/types/notification.ts
+++ backend/src/types/notification.ts
@@ -1,17 +1,24 @@
 import type { MastodonAccount } from 'wildebeest/backend/src/types/account'
 import type { MastodonStatus } from 'wildebeest/backend/src/types/status'
 
-export type NotificationType =
-	| 'mention'
-	| 'status'
-	| 'reblog'
-	| 'follow'
-	| 'follow_request'
-	| 'favourite'
-	| 'poll'
-	| 'update'
-	| 'admin.sign_up'
-	| 'admin.report'
+const notificationTypes = [
+	'mention',
+	'status',
+	'reblog',
+	'follow',
+	'follow_request',
+	'favourite',
+	'poll',
+	'update',
+	'admin.sign_up',
+	'admin.report',
+] as const
+
+export type NotificationType = (typeof notificationTypes)[number]
+
+export function isNotificationType(type: string): type is NotificationType {
+	return notificationTypes.includes(type as NotificationType)
+}
 
 export type Notification = {
 	id: string
@@ -20,20 +27,3 @@ export type Notification = {
 	account: MastodonAccount
 	status?: MastodonStatus
 }
-
-type ObjectRow = {
-	id: string
-	type: string
-	properties: string
-	mastodon_id: string
-	cdate: string
-	original_actor_id: string
-}
-
-export type NotificationsQueryResult = {
-	notif_actor_id: string
-	notif_type: NotificationType
-	notif_from_actor_id: string
-	notif_cdate: string
-	notif_id: string
-} & (ObjectRow | { id: null })
