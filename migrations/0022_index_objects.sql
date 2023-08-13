352dfa43faaa0733fc54fcb4112d681005872f18
shuymn <mail@shuymn.me>
2023-08-14 03:35:36 +0900

    Add index to objects table

---
+++ migrations/0022_index_objects.sql
@@ -0,0 +1,5 @@
+CREATE INDEX "objects_mastodon_id" ON "objects" ("mastodon_id");
+
+CREATE INDEX "objects_original_actor_id" ON "objects" ("original_actor_id");
+
+CREATE INDEX "objects_original_object_id" ON "objects" ("original_object_id");
--- schema.sql
+++ schema.sql
@@ -74,6 +74,12 @@ CREATE TABLE
     "local" INTEGER NOT NULL
   );
 
+CREATE INDEX "objects_mastodon_id" ON "objects" ("mastodon_id");
+
+CREATE INDEX "objects_original_actor_id" ON "objects" ("original_actor_id");
+
+CREATE INDEX "objects_original_object_id" ON "objects" ("original_object_id");
+
 CREATE TABLE
   inbox_objects (
     "id" TEXT PRIMARY KEY,
