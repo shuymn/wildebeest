9f1bf6a25672aa33cb8a7319aa8be2a9afd66cb8
shuymn <mail@shuymn.me>
2023-08-17 02:21:32 +0900

    Add table to store object history

--- schema.sql
+++ schema.sql
@@ -298,3 +298,15 @@ CREATE TABLE
   );
 
 CREATE INDEX "users_email" ON "users" ("email");
+
+CREATE TABLE
+  object_revisions (
+    "id" INTEGER NOT NULL PRIMARY KEY,
+    "type" TEXT NOT NULL GENERATED ALWAYS AS (JSON_EXTRACT("propertis", '$.type')) STORED,
+    "object_id" TEXT NOT NULL,
+    "properties" TEXT NOT NULL,
+    "cdate" DATETIME NOT NULL DEFAULT (STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW')),
+    CONSTRAINT "object_revisions_object_id_fkey" FOREIGN KEY ("object_id") REFERENCES "objects" ("id") ON DELETE CASCADE
+  );
+
+CREATE INDEX "object_revisions_object_id" ON "object_revisions" ("object_id");
