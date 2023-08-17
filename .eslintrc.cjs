7ddc7492ffe12f441367b1be10e4a14f2d5b4eed
shuymn <mail@shuymn.me>
2023-08-18 06:07:08 +0900

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -92,12 +92,12 @@ module.exports = {
 				  can pass with the current code, but we need to gradually
 				  re-enable most of them
 		*/
-		'@typescript-eslint/no-unsafe-assignment': 'off',
-		'@typescript-eslint/no-unsafe-argument': 'off',
-		'@typescript-eslint/no-unsafe-member-access': 'off',
-		'@typescript-eslint/restrict-plus-operands': 'off',
-		'@typescript-eslint/restrict-template-expressions': 'off',
-		'@typescript-eslint/no-unnecessary-type-assertion': 'off',
+		'@typescript-eslint/no-unsafe-assignment': 'warn',
+		'@typescript-eslint/no-unsafe-argument': 'warn',
+		'@typescript-eslint/no-unsafe-member-access': 'warn',
+		'@typescript-eslint/restrict-plus-operands': 'warn',
+		'@typescript-eslint/restrict-template-expressions': 'warn',
+		'@typescript-eslint/no-unnecessary-type-assertion': 'warn',
 		'@typescript-eslint/no-explicit-any': 'warn',
 	},
 }
