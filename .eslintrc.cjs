a66c1f2bf26dfbbf1529066e8ee4a415ae6d5b62
shuymn <mail@shuymn.me>
2023-08-18 06:29:53 +0900

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -57,7 +57,7 @@ module.exports = {
 						target: './backend/src/**/*',
 					},
 					{
-						from: './!(backend|config|node_modules|frontend/server)/**/*',
+						from: './!(backend|config|node_modules)/**/*',
 						target: './functions/**/*',
 					},
 					{
