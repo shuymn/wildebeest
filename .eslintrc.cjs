92938f11847089bf4dab15c077e8b3ecfca3dd1d
shuymn <mail@shuymn.me>
2023-08-18 06:06:40 +0900

    Use eslint-plugin-import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,14 +3,22 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:import/recommended',
+		'plugin:import/typescript',
 	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		tsconfigRootDir: __dirname,
 		project: ['./tsconfig.json'],
 	},
-	plugins: ['@typescript-eslint', 'unused-imports', 'simple-import-sort'],
+	plugins: ['@typescript-eslint', 'unused-imports', 'import'],
 	root: true,
+	settings: {
+		'import/resolver': {
+			typescript: true,
+			node: true,
+		},
+	},
 	rules: {
 		'prefer-const': 'error',
 		'no-var': 'error',
@@ -23,9 +31,62 @@ module.exports = {
 		'@typescript-eslint/no-unsafe-call': 'error',
 		'@typescript-eslint/await-thenable': 'error',
 		'@typescript-eslint/no-misused-promises': 'error',
-		'simple-import-sort/imports': 'error',
-		'simple-import-sort/exports': 'error',
 		'@typescript-eslint/no-unused-vars': 'off',
+		'import/no-duplicates': 'error',
+		'import/order': [
+			'error',
+			{
+				groups: ['builtin', 'external', 'internal'],
+				'newlines-between': 'always',
+				pathGroupsExcludedImportTypes: ['builtin'],
+				alphabetize: { order: 'asc', caseInsensitive: true },
+				pathGroups: [
+					{
+						pattern: 'wildebeest/**',
+						group: 'internal',
+					},
+				],
+			},
+		],
+		'import/no-restricted-paths': [
+			'error',
+			{
+				zones: [
+					{
+						from: './!(backend|config|node_modules)/**/*',
+						target: './backend/src/**/*',
+					},
+					{
+						from: './!(backend|config|node_modules)/**/*',
+						target: './functions/**/*',
+					},
+					{
+						from: './!(backend|do|node_modules)/**/*',
+						target: './do/**/*',
+					},
+					{
+						from: './!(backend|consumer|node_modules)/**/*',
+						target: './consumer/**/*',
+					},
+					{
+						from: './node_modules/@types/better-sqlite3',
+						target: './backend/src/**/*',
+					},
+					{
+						from: './node_modules/@types/better-sqlite3',
+						target: './functions/**/*',
+					},
+					{
+						from: './node_modules/@types/better-sqlite3',
+						target: './do/**/*',
+					},
+					{
+						from: './node_modules/@types/better-sqlite3',
+						target: './consumer/**/*',
+					},
+				],
+			},
+		],
 		/*
 			Note: the following rules have been set to off so that linting
 				  can pass with the current code, but we need to gradually
