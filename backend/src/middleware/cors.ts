f8998dbf35fd396dee4915f6e910f2f464dc61ad
shuymn <mail@shuymn.me>
2023-08-24 01:07:26 +0900

    Make CORS middleware configurable

--- backend/src/middleware/cors.ts
+++ backend/src/middleware/cors.ts
@@ -1,8 +1,21 @@
 import { cors } from 'hono/cors'
 
-export const corsMiddleware = () =>
-	cors({
+type AllowedMethod = 'GET' | 'PUT' | 'POST' | 'DELETE' | 'PATCH'
+type AllowedHeader = 'content-type' | 'authorization' | 'idempotency-key'
+
+type CORSOptions = {
+	origin: string | string[] | ((origin: string) => string | undefined | null)
+	allowMethods?: AllowedMethod[]
+	allowHeaders?: AllowedHeader[]
+	maxAge?: number
+	credentials?: boolean
+	exposeHeaders?: string[]
+}
+
+export const corsMiddleware = (
+	options: CORSOptions = {
 		origin: '*',
 		allowHeaders: ['content-type', 'authorization', 'idempotency-key'],
 		allowMethods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
-	})
+	}
+) => cors(options)
