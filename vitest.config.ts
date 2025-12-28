18d1044fe3c11fe2078938fa240b4815566ea649
Shu YAMANI <mail@shuymn.me>
2025-12-28 21:14:01 +0900

    refactor(vitest): move backend config to package

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,35 +1,11 @@
-import path from 'node:path'
+import { defineConfig } from 'vitest/config'
 
-import { defineWorkersProject, readD1Migrations } from '@cloudflare/vitest-pool-workers/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
-
-export default defineWorkersProject(async () => {
-	// Read all migrations in the `migrations` directory
-	const migrationsPath = path.join(__dirname, 'migrations')
-	const migrations = await readD1Migrations(migrationsPath)
-
-	return {
-		plugins: [tsconfigPaths()],
-		test: {
-			globals: true,
-			testTimeout: 30 * 1000,
-			include: ['./packages/backend/src/**/*.test.ts'],
-			includeSource: ['./packages/backend/src/**/*.ts'],
-			coverage: {
-				provider: 'istanbul',
-				reporter: ['json'],
-			},
-			setupFiles: ['./packages/backend/src/test/apply-migrations.ts'],
-			poolOptions: {
-				workers: {
-					singleWorker: true,
-					wrangler: { configPath: './wrangler.toml' },
-					miniflare: {
-						// Add a test-only binding for migrations, so we can apply them in a setup file
-						bindings: { TEST_MIGRATIONS: migrations },
-					},
-				},
-			},
+export default defineConfig({
+	test: {
+		projects: ['packages/backend/vitest.config.ts'],
+		coverage: {
+			provider: 'istanbul',
+			reporter: ['json'],
 		},
-	}
+	},
 })
