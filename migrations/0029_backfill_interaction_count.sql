a2f117cab25304749883f8910b7e9bc1f35a28e1
Shu YAMANI <mail@shuymn.me>
2026-06-08 13:02:32 +0900

    style(migrations): format subquery indentation

--- migrations/0029_backfill_interaction_count.sql
+++ migrations/0029_backfill_interaction_count.sql
@@ -6,11 +6,17 @@ SET interaction_count =
   (SELECT COUNT(*) FROM actor_favourites f
      JOIN actors fa ON f.actor_id = fa.id
      WHERE f.object_id = objects.id
-       AND EXISTS (SELECT 1 FROM users u WHERE u.actor_id = fa.id))
+       AND EXISTS (
+         SELECT 1 FROM users u
+         WHERE u.actor_id = fa.id
+       ))
   + (SELECT COUNT(*) FROM actor_reblogs r
      JOIN actors ra ON r.actor_id = ra.id
      WHERE r.object_id = objects.id
-       AND EXISTS (SELECT 1 FROM users u WHERE u.actor_id = ra.id))
+       AND EXISTS (
+         SELECT 1 FROM users u
+         WHERE u.actor_id = ra.id
+       ))
 WHERE local = 0;
 
 UPDATE actors
@@ -18,6 +24,9 @@ SET interaction_count =
   (SELECT COUNT(*) FROM actor_following af
      JOIN actors aa ON af.actor_id = aa.id
      WHERE af.target_actor_id = actors.id
-       AND EXISTS (SELECT 1 FROM users u WHERE u.actor_id = aa.id)
+       AND EXISTS (
+         SELECT 1 FROM users u
+         WHERE u.actor_id = aa.id
+       )
        AND af.state = 'accepted')
 WHERE NOT EXISTS (SELECT 1 FROM users u WHERE u.actor_id = actors.id);
